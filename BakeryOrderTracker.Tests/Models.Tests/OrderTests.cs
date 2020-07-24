5d6c5c8b0acaf3c0d6c993699be8a7f016e1c86c
Ben White <bwhite2140@outlook.com>
2020-07-24 13:22:04 -0700

    add passing test for returning properties

--- BakeryOrderTracker.Tests/Models.Tests/OrderTests.cs
+++ BakeryOrderTracker.Tests/Models.Tests/OrderTests.cs
@@ -14,5 +14,19 @@ namespace BakeryOrderTracker.Tests
       Order newOrder = new Order("name", "descriptive", 10, "1/1/2020");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetProperties_ReturnsCorrectValues_Order()
+    {
+      string name = "name";
+      string description = "descriptive";
+      int price = 5;
+      string date = "7/24/2020";
+      Order result = new Order(name, description, price, date);
+      Assert.AreEqual(name, result.Name);
+      Assert.AreEqual(description, result.Description);
+      Assert.AreEqual(price, result.Price);
+      Assert.AreEqual(date, result.Date);
+      Assert.AreEqual(1, result.Id);
+    }
   }
 }
